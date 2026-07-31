20111f23767f4e69612b051bc103a48bad646236
colt <colt@kilocode.dev>
2026-07-31 16:31:53 +0300

    build(jetbrains): auto-provision JDK 21 toolchain via foojay resolver

--- packages/kilo-jetbrains/settings.gradle.kts
+++ packages/kilo-jetbrains/settings.gradle.kts
@@ -12,3 +12,7 @@ pluginManagement {
         maven("https://packages.jetbrains.team/maven/p/ij/intellij-dependencies/")
     }
 }
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
